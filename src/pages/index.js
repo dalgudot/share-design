906f68556ebdfe49af663adc9d13d300925c280e
khkim <dalgudot@gmail.com>
2020-08-30 19:46:33 +0900

    Home Deploy Test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,65 +1,36 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { motion } from "framer-motion";
+import styled from "styled-components";
+import styles from "../styles/Home.module.css";
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <IntroText>
+        <h1 className={styles.name__text}>김경환</h1>
+        <span></span>
+        <h1>UI/UX 디자이너</h1>
+      </IntroText>
+      <h1 className={styles.share__text}>UI/UX 디자인 경험을 공유합니다.</h1>
+    </>
+  );
+};
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+export default Home;
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+const IntroText = styled.section`
+  width: 100%;
+  color: #eeeeee;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 90px;
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
-}
+  span {
+    width: 25vw;
+    height: 2px;
+    background-color: #eeeeee;
+    margin-left: 16px;
+    margin-right: 16px;
+    margin-top: 4px;
+  }
+`;
--- src/pages/test.js
+++
@@ -1,7 +0,0 @@
-import Spoqa from "../components/fonts-test";
-
-const testFonts = () => {
-  return <Spoqa />;
-};
-
-export default testFonts;
