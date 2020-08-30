5535ff4fff579de759adadbb8242269b60f14e8b
khkim <dalgudot@gmail.com>
2020-08-30 21:40:00 +0900

    subject component

---
+++ src/components/subject.js
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+const Subject = ({ order, title, date }) => {
+  return (
+    <>
+      <Container>
+        <LeftBox>
+          <p>{order}</p>
+          <h1>{title}</h1>
+        </LeftBox>
+        <p>{date}</p>
+      </Container>
+      <Line />
+    </>
+  );
+};
+
+export default Subject;
+
+const Container = styled.section`
+  width: 100%;
+  color: #eeeeee;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  margin-top: 30vh;
+
+  p {
+    font-size: 1.2vw;
+    font-weight: 300;
+  }
+
+  h1 {
+    font-weight: 400;
+    margin-left: 0.7vw;
+  }
+`;
+
+const LeftBox = styled.span`
+  display: flex;
+  align-items: flex-end;
+`;
+
+const Line = styled.div`
+  width: 100%;
+  height: 0.2vw;
+  background-color: #eeeeee;
+  margin-top: 3vh;
+`;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,25 @@
-import { motion } from "framer-motion";
+import Head from "next/head";
 import styled from "styled-components";
 import styles from "../styles/Home.module.css";
+import Subject from "../components/subject";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
     <>
+      <Head>
+        <title>KHK Portfolio</title>
+      </Head>
       <IntroText>
         <h1 className={styles.name__text}>김경환</h1>
         <span></span>
         <h1>UI/UX 디자이너</h1>
       </IntroText>
       <h1 className={styles.share__text}>UI/UX 디자인 경험을 공유합니다.</h1>
-      <Line />
+      <Subject order="001" title="Dark UI" date="2020.08.29 -" />
+      {/* 문자열만 "~"로 표기. 나머지는 {~} */}
+      <Subject order="002" title="Reponsive UI" date="2020.09.07 -" />
+      <Footer />
     </>
   );
 };
@@ -28,7 +36,7 @@ const IntroText = styled.section`
 
   span {
     width: 22vw;
-    height: 2px;
+    height: 0.2vw;
     background-color: #eeeeee;
     margin-left: 16px;
     margin-right: 17px;
@@ -36,9 +44,7 @@ const IntroText = styled.section`
   }
 `;
 
-const Line = styled.div`
+const Footer = styled.footer`
   width: 100%;
-  height: 2px;
-  background-color: #eeeeee;
-  margin-top: 30vh;
+  height: 500px;
 `;
