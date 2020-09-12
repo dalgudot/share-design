25540eeebaa9b7a171fb2fa2963bc05ec6c958b0
khkim <dalgudot@gmail.com>
2020-09-12 18:29:25 +0900

    twenties

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,50 +10,79 @@ const Home = () => {
     <>
       <Head>
         <title>KHK Portfolio</title>
+        <link rel="icon" href="/kh.ico" />
       </Head>
 
-      <IntroText>
-        <h1 className={styles.text__name}>김경환</h1>
-        {/* <span></span> */}
-        <h1>UI/UX 디자이너</h1>
-        <h1 className={styles.text__share}>UI/UX 디자인 경험을</h1>
-        <h1 className={styles.text__share}>공유합니다.</h1>
+      <Container>
+        <IntroText>
+          <h1 className={styles.text__name}>김경환</h1>
+          <h1>UI/UX 디자이너</h1>
+          <h1 className={styles.text__share}>UI/UX 디자인 경험을</h1>
+          <h1 className={styles.text__share}>공유합니다.</h1>
+        </IntroText>
+
         {/* 문자열만 "~"로 표기. 나머지는 {~} */}
-      </IntroText>
+        <Subject order="001" title="반응형 UI 디자인" date="2020.09.06 - " />
+        <div className={styles.align__center}>
+          <Link href="/twenties">
+            <a>
+              <button className={styles.button__neumorphism}>
+                20대를 남기다
+              </button>
+            </a>
+          </Link>
+        </div>
 
-      <Subject order="001" title="반응형 UI 디자인" date="2020.09.06 - " />
-      <div className={styles.align__center}>
-        <Link href="/twenties">
-          <a>
-            <button className={styles.button__neumorphism}>
-              20대를 남기다
-            </button>
-          </a>
-        </Link>
-      </div>
+        <Subject order="002" title="다크 모드" date="2020.00.00 -" />
 
-      <Subject order="002" title="다크 모드" date="2020.00.00 -" />
-
-      <Footer />
+        <Footer />
+      </Container>
     </>
   );
 };
 
 export default Home;
 
+const Container = styled.div`
+  margin: 0 4.5vw;
+
+  h1 {
+    font-weight: 700;
+  }
+
+  @media all and (min-width: 320px) {
+    .button__neumorphism {
+      font-size: 18px;
+    }
+
+    h1 {
+      font-size: 8vw;
+    }
+  }
+
+  @media all and (min-width: 768px) {
+    h1 {
+      font-size: 6vw;
+    }
+  }
+
+  @media all and (min-width: 1024px) {
+    h1 {
+      font-size: 4.5vw;
+    }
+  }
+
+  @media all and (min-width: 1440px) {
+    h1 {
+      font-size: 4.2vw;
+    }
+  }
+`;
+
 const IntroText = styled.section`
   width: 100%;
-  color: #eeeeee;
+  color: var(--white);
   margin-top: 12vh;
-
-  /* span {
-    width: 22vw;
-    height: 0.2vw;
-    background-color: #eeeeee;
-    margin-left: 16px;
-    margin-right: 17px;
-    margin-top: 4px;
-  } */
 `;
 
 const Footer = styled.footer`
--- src/pages/twenties.js
+++ src/pages/twenties.js
@@ -3,20 +3,102 @@ import Link from "next/link";
 import styles from "../styles/Twenties.module.css";
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { ParallaxProvider, Parallax } from "react-scroll-parallax";
 
 const Twenties = () => {
   return (
     <>
       <Head>
-        <title>Twenties</title>
+        <title>20대를 남기다</title>
+        <link rel="icon" href="/twenties.ico" />
       </Head>
-      <Link href="/">
-        <a>
-          <h1 className={styles.text__color__white}>20대를 남기다</h1>
-        </a>
-      </Link>
+
+      {/* <Link href="/">
+        <a>Back</a>
+      </Link> */}
+
+      {/* react-scroll-parallax */}
+      {/* https://www.npmjs.com/package/react-scroll-parallax */}
+
+      <TextContainer>
+        <div>
+          <p>20살</p>
+        </div>
+      </TextContainer>
+
+      <ParallaxProvider>
+        {/* [y1, y2] -> style="transform: translate3d(0%, y1 -> y2, 0px); */}
+        {/* y의 변화 */}
+        <Parallax y={[-40, 15]} tagOuter="figure">
+          <TextContainer>
+            <div>
+              <p>참 설렜다</p>
+            </div>
+          </TextContainer>
+        </Parallax>
+
+        <Parallax y={[15, -40]} tagOuter="figure">
+          <TextContainer>
+            <div>
+              <p>20대라는 게</p>
+              <p>좋았다</p>
+            </div>
+          </TextContainer>
+        </Parallax>
+
+        <Parallax y={[15, -40]} tagOuter="figure">
+          <TextContainer>
+            <div>
+              <p>하지만</p>
+            </div>
+          </TextContainer>
+        </Parallax>
+
+        <Parallax y={[-40, 15]} tagOuter="figure">
+          <TextContainer>
+            <div>
+              <p>20대는 내 생각과 달랐다</p>
+            </div>
+          </TextContainer>
+        </Parallax>
+
+        <Parallax y={[15, -40]} tagOuter="figure">
+          <TextContainer>
+            <img src="/images/a.jpg" />
+          </TextContainer>
+        </Parallax>
+      </ParallaxProvider>
     </>
   );
 };
 
 export default Twenties;
+
+const TextContainer = styled.main`
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  div {
+    width: auto;
+    height: auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  p {
+    color: var(--white);
+    font-size: 48px;
+    font-weight: 100;
+  }
+
+  img {
+    width: 100vw;
+    height: 100vh;
+  }
+`;
