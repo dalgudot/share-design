4d789e9039889447e96ef3606b69c4db7dee4972
khkim <dalgudot@gmail.com>
2020-09-20 20:20:46 +0900

    draft

--- src/pages/twenties-gallary.js
+++ src/pages/twenties-gallary.js
@@ -68,22 +68,263 @@ const TwentiesGallary = () => {
 
         {/* <ParallaxImage img="/images/a.jpg" amountFigure="-0.1" /> */}
 
-        <ParallaxImage imgOne="/images/a.jpg" amountOne={0.1} />
+        <ParallaxImage imgOne="/images/a.jpg" amountOne={0} />
 
         <ParallaxText
           yOne={15}
           yTwo={-10}
           textOne="이대로"
-          textTwo="내 20대가"
-          textThree="끝이 나버리면"
-          textFour="어떡하지?"
+          textTwo=""
+          textThree=""
+          textFour=""
         />
+
+        <ParallaxText
+          yOne={-10}
+          yTwo={15}
+          textOne="내 20대가"
+          textTwo=""
+          textThree=""
+          textFour=""
+        />
+
+        <ParallaxText
+          yOne={-10}
+          yTwo={15}
+          textOne="끝이 나버리면"
+          textTwo="어떡하지?"
+          textThree=""
+          textFour=""
+        />
+
+        <ParallaxImage imgOne="/images/a.jpg" amountOne={0} />
 
         <ParallaxText
           yOne={-10}
           yTwo={15}
           textOne="나는 앞으로"
           textTwo="무엇을 하게 될까?"
+          textThree=""
+          textFour=""
+        />
+
+        <ParallaxText
+          yOne={-10}
+          yTwo={15}
+          textOne="불안했다"
+          textTwo=""
+          textThree=""
+          textFour=""
+        />
+
+        <ParallaxText
+          yOne={-10}
+          yTwo={15}
+          textOne="정해진 건"
+          textTwo="아무것도 없었다"
+          textThree=""
+          textFour=""
+        />
+
+        <ParallaxText
+          yOne={-10}
+          yTwo={15}
+          textOne="이 선택이"
+          textTwo="맞는 걸까?"
+          textThree=""
+          textFour=""
+        />
+
+        <ParallaxText
+          yOne={-10}
+          yTwo={15}
+          textOne="이렇게 해도"
+          textTwo="괜찮은 걸까?"
+          textThree=""
+          textFour=""
+        />
+
+        <ParallaxText
+          yOne={-10}
+          yTwo={15}
+          textOne="이 길이"
+          textTwo="맞는 걸까?"
+          textThree=""
+          textFour=""
+        />
+
+        <ParallaxImage imgOne="/images/a.jpg" amountOne={0} />
+
+        <ParallaxText
+          yOne={-10}
+          yTwo={15}
+          textOne="방향을 잃었다"
+          textTwo=""
+          textThree=""
+          textFour=""
+        />
+
+        <ParallaxImage imgOne="/images/a.jpg" amountOne={0} />
+
+        <ParallaxText
+          yOne={-10}
+          yTwo={15}
+          textOne="나는"
+          textTwo=""
+          textThree=""
+          textFour=""
+        />
+
+        <ParallaxText
+          yOne={-10}
+          yTwo={15}
+          textOne="무엇을 향해"
+          textTwo="달리고 있는 걸까"
+          textThree=""
+          textFour=""
+        />
+
+        <ParallaxImage imgOne="/images/a.jpg" amountOne={0} />
+
+        <ParallaxText
+          yOne={-10}
+          yTwo={15}
+          textOne="나는"
+          textTwo=""
+          textThree=""
+          textFour=""
+        />
+
+        <ParallaxText
+          yOne={-10}
+          yTwo={15}
+          textOne="지금 무얼 하고"
+          textTwo="있는 걸까"
+          textThree=""
+          textFour=""
+        />
+
+        <ParallaxImage imgOne="/images/a.jpg" amountOne={0} />
+
+        <ParallaxText
+          yOne={-10}
+          yTwo={15}
+          textOne="무작정 달리고만"
+          textTwo="있는 건 아닐지"
+          textThree=""
+          textFour=""
+        />
+
+        <ParallaxText
+          yOne={-10}
+          yTwo={15}
+          textOne="내가 아닌"
+          textTwo=""
+          textThree=""
+          textFour=""
+        />
+
+        <ParallaxText
+          yOne={-10}
+          yTwo={15}
+          textOne="남들의"
+          textTwo="시선 안에서"
+          textThree=""
+          textFour=""
+        />
+
+        <ParallaxText
+          yOne={-10}
+          yTwo={15}
+          textOne="살고 있는 건"
+          textTwo="아닐지"
+          textThree=""
+          textFour=""
+        />
+
+        <ParallaxText
+          yOne={-10}
+          yTwo={15}
+          textOne="그건"
+          textTwo="나에게"
+          textThree=""
+          textFour=""
+        />
+
+        <ParallaxText
+          yOne={-10}
+          yTwo={15}
+          textOne="너무"
+          textTwo="미안한 일이"
+          textThree="아닐지"
+          textFour=""
+        />
+
+        <ParallaxImage imgOne="/images/a.jpg" amountOne={0} />
+
+        <ParallaxText
+          yOne={-10}
+          yTwo={15}
+          textOne="조금은 쉬어가도"
+          textTwo="되지 않을까"
+          textThree=""
+          textFour=""
+        />
+
+        <ParallaxText
+          yOne={-10}
+          yTwo={15}
+          textOne="가장 아름다울"
+          textTwo="나의 20대를"
+          textThree=""
+          textFour=""
+        />
+
+        <ParallaxText
+          yOne={-10}
+          yTwo={15}
+          textOne="나를 위해"
+          textTwo="살아야 하지 않을까"
+          textThree=""
+          textFour=""
+        />
+
+        <ParallaxText
+          yOne={-10}
+          yTwo={15}
+          textOne="나에게"
+          textTwo="미안하지 않도록"
+          textThree=""
+          textFour=""
+        />
+
+        <ParallaxImage imgOne="/images/a.jpg" amountOne={0} />
+
+        <ParallaxText
+          yOne={-10}
+          yTwo={15}
+          textOne="그게 뭐가"
+          textTwo="그렇게 어렵다고"
+          textThree=""
+          textFour=""
+        />
+
+        <ParallaxText
+          yOne={-10}
+          yTwo={15}
+          textOne="조금만"
+          textTwo="내려놓으면 되는데"
+          textThree=""
+          textFour=""
+        />
+
+        <ParallaxImage imgOne="/images/a.jpg" amountOne={0} />
+
+        <ParallaxText
+          yOne={-10}
+          yTwo={15}
+          textOne="20대, 그대"
+          textTwo=""
           textThree=""
           textFour=""
         />
