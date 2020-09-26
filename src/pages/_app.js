0e3c118d204881931d546f57e7b6a420b0421da4
khkim <dalgudot@gmail.com>
2020-09-26 20:04:45 +0900

    test

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,10 +2,20 @@ import "../styles/globals.css";
 import App from "next/app";
 import { appWithTranslation } from "../../i18n";
 
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
 
-MyApp.getInitialProps = async (appContext) => ({
-  ...(await App.getInitialProps(appContext)),
-});
+export default MyApp;
 
-export default appWithTranslation(MyApp);
+// import "../styles/globals.css";
+// import App from "next/app";
+// import { appWithTranslation } from "../../i18n";
+
+// const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
+
+// MyApp.getInitialProps = async (appContext) => ({
+//   ...(await App.getInitialProps(appContext)),
+// });
+
+// export default appWithTranslation(MyApp);
