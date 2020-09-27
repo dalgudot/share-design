f14de5cc9faff199aa06112ff10c85882bbe1e35
khkim <dalgudot@gmail.com>
2020-09-27 15:55:32 +0900

    redux study

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -11,38 +11,3 @@ export default function App({ Component, pageProps }) {
     </Provider>
   );
 }
-
-// import "../styles/globals.css";
-// import React from "react";
-// import App from "next/app";
-// import { appWithTranslation } from "../../i18n";
-
-// function MyApp({ Component, pageProps }) {
-//   return <Component {...pageProps} />;
-// }
-
-// MyApp.getInitialProps = async (appContext) => {
-//   const appProps = await App.getInitialProps(appContext);
-//   return { ...appProps };
-// };
-
-// export default appWithTranslation(MyApp);
-
-// import App from "next/app";
-// import { appWithTranslation } from "/i18n";
-
-// const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
-
-// MyApp.getInitialProps = async (appContext) => ({
-//   ...(await App.getInitialProps(appContext)),
-// });
-
-// export default appWithTranslation(MyApp);
-
-// import "../styles/globals.css";
-
-// function MyApp({ Component, pageProps }) {
-//   return <Component {...pageProps} />;
-// }
-
-// export default MyApp;
