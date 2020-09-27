f91849152d41c5f6e11e2f37f7bab65b176d41ee
khkim <dalgudot@gmail.com>
2020-09-27 15:16:09 +0900

    test2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,10 +7,10 @@ function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
-MyApp.getInitialProps = async (appContext) => {
-  const appProps = await App.getInitialProps(appContext);
-  return { ...appProps };
-};
+// MyApp.getInitialProps = async (appContext) => {
+//   const appProps = await App.getInitialProps(appContext);
+//   return { ...appProps };
+// };
 
 export default appWithTranslation(MyApp);
 
