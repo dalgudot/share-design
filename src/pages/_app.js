3fa859109e297870007dc227799399aa410f3e69
khkim <dalgudot@gmail.com>
2020-09-27 21:58:04 +0900

    0927 End

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import { Provider } from "react-redux";
-import { useStore } from "../../store";
+import { useStore } from "../store/store";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
