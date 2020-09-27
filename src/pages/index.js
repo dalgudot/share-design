4731cc9e99328418444409b1f76f3662dee90b90
khkim <dalgudot@gmail.com>
2020-09-27 20:39:46 +0900

    original

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,8 @@
-import { useSelector, shallowEqual, useDispatch } from "react-redux";
+import HeadInfo from "../components/head-info";
+import Link from "next/link";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { useSelector, useDispatch } from "react-redux";
 
 const useLanguageChange = () => {
   const lang = useSelector((state) => state.language);
@@ -10,23 +14,70 @@ const useLanguageChange = () => {
   return { lang, languageChange };
 };
 
-const LangChan = () => {
+const Index = () => {
   const { lang, languageChange } = useLanguageChange();
   return (
     <>
-      <h1>{lang}</h1>;<button onClick={languageChange}>언어 변환</button>
+      <HeadInfo
+        title="KHK DESIGN"
+        icoImage="/kh.ico"
+        description="UI/UX 디자인 경험을 공유합니다"
+        url="https://khk-design.kr/"
+        thumbnail="/images/a.jpg"
+      />
+
+      <CenterAlign>
+        <Link href="twenties-gallary">
+          <a>
+            <motion.button
+              whileHover={{ scale: 1.1 }}
+              transition={{ type: "spring", stiffness: 120, duration: 0.2 }}
+            >
+              20대를 남기다
+            </motion.button>
+          </a>
+        </Link>
+      </CenterAlign>
+
+      <Button_Language>
+        <button onClick={languageChange}>{lang}</button>
+      </Button_Language>
     </>
   );
 };
 
-export default LangChan;
+export default Index;
 
-export function getStaticProps() {
-  return {
-    props: {
-      initialReduxState: {
-        language: "ko",
-      },
-    },
-  };
-}
+const Button_Language = styled.section`
+  position: fixed;
+  top: 24px;
+  right: 24px;
+  button {
+    margin-right: -1px;
+    font-size: 14px;
+    font-weight: 100;
+    color: var(--white);
+    padding: 6px 12px;
+    background-color: transparent;
+    border: solid 1px var(--white);
+  }
+`;
+
+const CenterAlign = styled.section`
+  width: 100vw;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  button {
+    font-size: 24px;
+    font-weight: 100;
+    color: var(--white);
+    padding: 18px 36px;
+    background-color: transparent;
+    border: solid 1px var(--white);
+  }
+`;
