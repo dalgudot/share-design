584a3e0f1ee40750f8ee46f953f3878bfdb42036
khkim <dalgudot@gmail.com>
2020-09-27 15:38:57 +0900

    redux test

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,32 @@
+import { Provider } from "react-redux";
+import { useStore } from "../../store";
 import "../styles/globals.css";
-import React from "react";
-import App from "next/app";
-import { appWithTranslation } from "../../i18n";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }) {
+  const store = useStore(pageProps.initialReduxState);
+
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
+
+// import "../styles/globals.css";
+// import React from "react";
+// import App from "next/app";
+// import { appWithTranslation } from "../../i18n";
+
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />;
+// }
 
 // MyApp.getInitialProps = async (appContext) => {
 //   const appProps = await App.getInitialProps(appContext);
 //   return { ...appProps };
 // };
 
-export default appWithTranslation(MyApp);
+// export default appWithTranslation(MyApp);
 
 // import App from "next/app";
 // import { appWithTranslation } from "/i18n";
