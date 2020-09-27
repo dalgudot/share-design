90ca9d65651d5ab95a713e8514418b4ebb7306b7
khkim <dalgudot@gmail.com>
2020-09-27 15:41:11 +0900

    re test

--- src/pages/ssr.js
+++ src/pages/ssr.js
@@ -1,5 +1,5 @@
 import Page from "../components/page";
-import { initializeStore } from "../store";
+import { initializeStore } from "../../store";
 
 export default function SSR() {
   return <Page />;
