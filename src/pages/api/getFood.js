28c7d4c8e2c9ce8784dbe46876e319047e656f49
khkim <dalgudot@gmail.com>
2020-11-01 12:59:11 +0900

    add firebase

--- src/pages/api/getFood.js
+++ src/pages/api/getFood.js
@@ -1,4 +1,4 @@
-import { verifyIdToken } from "../../utils/auth/firebaseAdmin";
+import { verifyIdToken } from "../../../utils/auth/firebaseAdmin";
 
 const favoriteFoods = ["pizza", "burger", "chips", "tortilla"];
 
