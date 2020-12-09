a0c7c9ffd6062c5cea4bfa5c91819710f629ae19
khkim <dalgudot@gmail.com>
2020-12-09 20:39:13 +0900

    20201209

--- src/pages/contents/two-language.js
+++ src/pages/contents/two-language.js
@@ -1,7 +1,7 @@
 import HeadInfo from '../../components/head-info/head-info';
 import { indexInfo } from '../../components/head-info/head-info-text';
 import Link from 'next/link';
-import Text from '../../components/typo/text';
+import H1Text from '../../components/typo/h1';
 import { fontSize, fontWeight } from '../../components/typo/font';
 import { DetectBrowserLang } from '../../components/func/detect-browser-lang';
 
@@ -12,14 +12,14 @@ const TwoLanguage = () => {
     <>
       <HeadInfo info={indexInfo} />
 
-      <Text
+      <H1Text
         text="제목이 들어갑니다."
         mobileSize={fontSize[18]}
         tabletSize={fontSize[20]}
         desktopSize={fontSize[20]}
         weight={fontWeight[700]}
       />
-      <Text
+      <H1Text
         text="본문이 들어갑니다."
         mobileSize={fontSize[18]}
         tabletSize={fontSize[20]}
