84e382c458058a425f14410ca17e28700296e897
khkim <dalgudot@gmail.com>
2021-01-05 21:18:54 +0900

    20200105

--- src/lib/funtions/prevent-illegal-theft.tsx
+++ src/lib/funtions/prevent-illegal-theft.tsx
@@ -5,25 +5,27 @@ export function PreventIllegalTheft() {
     //마우스 오른쪽 클릭만 막기
     //https://developer.mozilla.org/en-US/docs/Web/API/Element/contextmenu_event
 
+    // 마우스 오른쪽 클릭(oncontextmenu)
     let contextmenu: Function = () => {
       return document.oncontextmenu;
     };
 
+    // 왼쪽 마우스 이미지 드래그(ondragstart)
     let dragstart: Function = () => {
       return document.ondragstart;
     };
 
+    // 왼쪽 마우스 문자 드래그(onselectstart)
     let selectstart: Function = () => {
       return document.onselectstart;
     };
 
-    let keydown: Function = () => {
-      return document.onkeydown;
-    };
+    // 키보드 단축키 복사(onkeydown) 막기
+    // let keydown: Function = () => {
+    //   return document.onkeydown;
+    // };
 
-    contextmenu = dragstart = selectstart = keydown = new Function(
-      'return false'
-    );
+    contextmenu = dragstart = selectstart = new Function('return false');
 
     // contextmenu = document.ondragstart = document.onselectstart = document.onkeydown = new Function(
     //   'return false'
