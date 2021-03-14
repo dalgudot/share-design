ce02361e92e7563436e28fc12b05a1336a8f89cf
khkim <dalgudot@gmail.com>
2021-03-14 17:01:51 +0900

    modify article-1 info

--- src/elements/head-info/article/ui-ux-design/1-info.ts
+++ src/elements/head-info/article/ui-ux-design/1-info.ts
@@ -20,8 +20,8 @@ export const uiUxDesignInfo = {
 
   description: {
     k:
-      '디자인 공유하기는 전 세계 사람들에게 UI/UX 디자인 경험을 공유하기 위한 공간이다. 내가 글을 쓸 수 있는 한국어와 영어 두 언어로 콘텐츠를 제공하고 있다. 영어가 익숙한 사용자가 한국어로 된 디자인 공유하기에 들어왔거나, 한국어가...',
+      '디자인 공유하기는 전 세계 사람들에게 UI/UX 디자인 경험을 공유하기 위한 공간이다. 내가 글을 쓸 수 있는 한국어와 영어 두 언어로 콘텐츠를 제공하고 있다. 영어가 익숙한 사용자가 한국어로 된 디자인 공유하기에 들어왔거나, 한국어가 익숙한 사용자가 영어로 된 디자인 공유하기에 들어온 상황을 대비해 디자인 공유하기에서는 모든 화면의 상단 헤더(Header)에서 언어를 전환할 수 있는 토글 버튼을 제공하고 있다.',
     e:
-      'SHARE DESIGN is a space for sharing UI/UX design experiences with people around the world. Contents are provided in both Korean and English, where I can write. In case a user who is familiar with English has entered to share a design in Korean...',
+      'SHARE DESIGN is a space for sharing UI/UX design experience with people around the world. Contents are provided in both Korean and English, where I can write. In case a user familiar with English entered the SHARE DESIGN in Korean, or a user familiar with Korean entered the SHARE DESIGN in English, SHARE DESIGN provides a toggle button that allows user to switch languages at the top header of all screens.',
   },
 };
