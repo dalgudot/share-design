4e3ec89aacd3bc5fb6c1ace16baf4595647ae00c
khkim <dalgudot@gmail.com>
2021-04-19 07:25:03 +0900

    cpt Korean contents ui-ux-deisgn-02

--- src/data/article/ui-ux-design/2.ts
+++ src/data/article/ui-ux-design/2.ts
@@ -21,7 +21,7 @@ export const uiUxDesign2 = () => {
 
     summary: {
       k:
-        "앱이나 웹 같은 디지털 프로덕트(Digital Product, 이하 프로덕트)에서 모션(Motion, 움직임)은 사용자의 시선을 끌거나, 사용자가 한 행동에 반응을 보여주는 좋은 방법이다. UI(User Interface, 사용자 인터페이스)를 사용자와 소통하도록 디자인하는 '인터랙션 디자인(Interaction Design)'의 방법론 중 하나...",
+        "앱이나 웹 같은 디지털 프로덕트(Digital Product)에서 모션(Motion, 움직임)은 사용자의 시선을 끌거나, 사용자가 한 행동에 반응을 보여주는 좋은 방법이다. UI(User Interface, 사용자 인터페이스)를 사용자와 소통하도록 디자인하는 '인터랙션 디자인(Interaction Design)'의 방법론 중 하나...",
       e:
         "In a digital product like apps or webs, motion is a great way to grab a user's attention or show a reaction to a user's actions. It can be seen as one of the methodologies of 'Interaction Design'...",
     },
@@ -125,7 +125,7 @@ export const uiUxDesign2 = () => {
     contents: [
       {
         k:
-          "앱이나 웹 같은 디지털 프로덕트(Digital Product, 이하 프로덕트)에서 모션(Motion, 움직임)은 사용자의 시선을 끌거나, 사용자가 한 행동에 반응을 보여주는 좋은 방법이다. UI(User Interface, 사용자 인터페이스)를 사용자와 소통하도록 디자인하는 '인터랙션 디자인(Interaction Design)'의 방법론 중 하나로 볼 수 있다.",
+          "앱이나 웹 같은 디지털 프로덕트(Digital Product)에서 모션(Motion, 움직임)은 사용자의 시선을 끌거나, 사용자가 한 행동에 반응을 보여주는 좋은 방법이다. UI(User Interface, 사용자 인터페이스)를 사용자와 소통하도록 디자인하는 '인터랙션 디자인(Interaction Design)'의 방법론 중 하나로 볼 수 있다.",
         e:
           "In a digital product like apps or webs, motion is a great way to grab a user's attention or show a reaction to a user's actions. It can be seen as one of the methodologies of 'Interaction Design', which designs UI(User Interface) to communicate with users.",
       },
@@ -139,9 +139,9 @@ export const uiUxDesign2 = () => {
       },
       {
         k:
-          "컴퓨터 키보드처럼 누르면 '눌리는' 반응을 느낄 수 있는 물리적인 UI와는 다르게, 스마트폰 앱 같은 터치 기반 디지털 프로덕트의 UI에서는 사용자가 느낄 수 있는 반응이 한정적이다. 소리나 진동 반응 등이 그것인데, 동작 환경에 따라 사용이 제한적이며 표현 범위의 한계가 있다.",
+          "컴퓨터 키보드처럼 누르면 '눌리는' 반응을 느낄 수 있는 물리적인 UI와는 다르게, 스마트폰 앱 같은 터치 기반 디지털 프로덕트의 UI에서는 사용자가 느낄 수 있는 반응이 한정적이다. 소리나 진동 등이 그것인데, 동작 환경에 따라 사용이 제한적이며 표현 범위의 한계가 있다.",
         e:
-          "Unlike a physical UI where you can feel a 'pressed' response like a computer keyboard, the UI of a touch-based digital product such as a smartphone app has a limited response that a user can feel. These include sound and vibration reactions, and their use is limited depending on the operating environment and the range of expressions is limited.",
+          "Unlike a physical UI where you can feel a 'pressed' response like a computer keyboard, the UI of a touch-based digital product such as a smartphone app has a limited response that a user can feel. These include sound and vibration, and their use is limited depending on the operating environment and the range of expressions is limited.",
       },
       {
         k:
@@ -151,31 +151,31 @@ export const uiUxDesign2 = () => {
       },
       {
         k:
-          "\u00A0\u00A0\u00A0사실 디지털 프로덕트의 UI에서 모션 디자인은 인터랙션 디자인의 방법론 중 하나일 뿐이지만, 요즘에는 인터랙션 디자인이 곧 모션 디자인이라고 봐도 될 만큼 UI 디자인에서 모션 디자인의 역할은 중요하다. 프로덕트 회의에서 '인터랙션 디자인'이라는 단어가 나오면 이는 '모션 디자인'이라고 봐도 될 정도다.",
+          "\u00A0\u00A0\u00A0디지털 프로덕트의 UI에서 모션 디자인은 인터랙션 디자인의 방법론 중 하나일 뿐이지만, 요즘에는 인터랙션 디자인이 곧 모션 디자인이라고 봐도 될 만큼 디지털 프로덕트의 UI 디자인에서 모션 디자인의 역할은 중요하다. 프로덕트 회의에서 '인터랙션 디자인'이라는 단어가 나오면 이는 '모션 디자인'이라고 봐도 될 정도다.",
         e:
-          "\u00A0\u00A0\u00A0In fact, in the UI of digital products, motion design is only one of the methodologies of interaction design, but these days, the role of motion design in UI design is so important that interaction design can be considered as motion design. When the word 'interaction design' comes up at a product meeting, it can be considered as 'motion design'",
+          "\u00A0\u00A0\u00A0In the UI of digital products, motion design is only one of the methodologies of interaction design, but these days, the role of motion design in UI design is so important that interaction design can be considered as motion design. When the word 'interaction design' comes up at a product meeting, it can be considered as 'motion design'",
       },
       {
         k:
-          '\u00A0\u00A0\u00A0그런데 모션 디자인은 사용자 경험(UX, User eXperience)에 어떤 영향을 미칠까? 꽤 오랫동안 고민한 주제다. 사람은 본능적으로 움직이는 무언가가 있는 쪽으로 눈이 간다. 디지털 프로덕트에서도 마찬가지다. 사용자가 프로덕트를 쓰면서 움직이는 요소가 있다면 그쪽으로 시선이 간다. 아래는 사용자가 스크롤을 내리다가 특정 UI 요소가 화면 안에 들어오면 모션이 동작해 사용자의 시선을 끄는 디자인이다.',
+          '\u00A0\u00A0\u00A0그런데 모션 디자인은 사용자 경험(UX, User eXperience)에 어떤 영향을 미칠까? 꽤 오랫동안 고민한 주제다. 사람은 본능적으로 움직이는 무언가가 있는 쪽으로 눈이 간다. 디지털 프로덕트에서도 마찬가지다. 프로덕트를 쓰면서 움직이는 요소가 있다면 그쪽으로 시선이 간다. 아래는 사용자가 스크롤을 내리다가 특정 UI 요소가 화면 안에 들어오면 모션이 동작해 사용자의 시선을 끄는 디자인이다.',
         e:
           "\u00A0\u00A0\u00A0But how does motion design affect UX? It's a topic that I've been thinking about for quite some time. People instinctively turn their eyes toward something that moves. The same is true for digital products. If there is an element that moves while the user is using the product, the gaze is directed towards it. Below is a design that catches the user's attention by moving a motion when a specific UI element enters the screen while the user scrolls down.",
       },
       {
         k:
-          '과연 이 디자인이 좋은 UX를 만드는 디자인이라 할 수 있을까? 사용자가 스크롤을 내리다가 	특정 UI 요소가 있는 영역에 도달하면 모션을 보여줘 사용자의 시선을 끈 것이 과연 사용자에게 좋은 UX를 주는 걸까?',
+          '과연 이 디자인이 좋은 UX를 만드는 디자인이라 할 수 있을까? 사용자가 스크롤을 내리다가 특정 UI 요소가 있는 영역에 도달하면 모션을 보여줘 사용자의 시선을 끈 것이 과연 사용자에게 좋은 UX를 주는 걸까?',
         e:
           "Can this design be said to be a design that makes a good UX? When the user scrolls down and reaches the area where a specific UI element is located, does it really give a good UX to the user by showing a motion to attract the user's attention?",
       },
       {
         k:
-          '\u00A0\u00A0\u00A0난 모션이 일어나는 UI 요소가 사용자가 꼭 알아야 하는 정보라면 좋은 경험을 줄 수 있는 디자인이고, 불필요하게 멋을 부리는 거라면 사용자에게 불필요한 시선 이동을 강요한 좋지 않은 디자인이라 생각한다. 즉 디자이너의 의도에 따라 이 모션 디자인은 좋은 디자인이 될 수도, 좋지 않은 디자인이 될 수도 있다.',
+          '\u00A0\u00A0\u00A0난 모션이 일어나는 UI 요소에 사용자가 꼭 알아야 하는 정보가 포함돼 있다면 좋은 경험을 줄 수 있는 디자인이고, 그저 멋을 부리는 거라면 사용자에게 불필요한 시선 이동을 강요한 좋지 않은 디자인이라 생각한다. 즉 디자이너의 의도에 따라 이 모션 디자인은 좋은 디자인이 될 수도, 좋지 않은 디자인이 될 수도 있다.',
         e:
           "\u00A0\u00A0\u00A0I think that if the UI element in which motion occurs is information that the user must know, it is a design that can give a good experience, and if it is unnecessarily stylish, it is a bad design that forces the user to move unnecessary gaze. In other words, depending on the designer's intention, this motion design can be either a good design or a bad design.",
       },
       {
         k:
-          '\u00A0\u00A0\u00A0하지만 디자이너로서 내적 만족을 위해 재미있는 모션을 디자인해 사용자에게 제공하고 싶은 욕구가 있는 것도 사실이다. 예를 들면 아래는 내가 UI/UX 디자이너로 일하고 있는 중고 명품 플랫폼 쿠돈에서 현재(2021년 4월 18일 기준) 진행하고 있는 리뷰(Review) 디자인의 일부다. 별을 누르면 별 위쪽에 있는 문구는 작아지면서 사라지고, 별은 y축을 기준으로 720도를 회전하면서 위쪽으로 이동하고 사용자가 누른 점수에 맞춰 점수만큼 별의 색이 재빠르게 채워진다.',
+          '\u00A0\u00A0\u00A0하지만 디자이너로서 내적 만족을 위해 재미있는 모션을 디자인해 사용자에게 제공하고 싶은 욕구가 있는 것도 사실이다. 예를 들면 아래는 내가 UI/UX 디자이너로 일하고 있는 중고 명품 플랫폼 쿠돈에서 현재(2021년 4월 18일 기준) 진행하고 있는 리뷰(Review) 디자인의 일부다. 별을 누르면 별 위쪽에 있는 문구는 작아지면서 사라진다. 동시에 별은 y축을 기준으로 720도를 회전하면서 위쪽으로 이동하고, 사용자가 누른 점수에 맞춰 점수만큼 별의 색이 재빠르게 채워진다.',
         e:
           '\u00A0\u00A0\u00A0However, as a designer, it is also true that there is a desire to design interesting motions for internal satisfaction and provide them to users. For example, the following is a part of a review design currently underway(as of April 18, 2021) at Koodon, a used luxury goods platform in South Korea where I work as a UI/UX designer. When you press the star, the text above the star gets smaller and disappears, the star rotates 720 degrees on the y-axis and moves upward, and the color of the star is quickly filled as much as the score according to the score the user pressed.',
       },
@@ -187,7 +187,7 @@ export const uiUxDesign2 = () => {
       },
       {
         k:
-          "\u00A0\u00A0\u00A0사실 이 모션은 쿠돈의 브랜드 키워드 3가지 '순환시키는, 도움이 되는, 재빠른' 중 '순환시키는'과 '재빠른'을 모션으로 실체화한 것이다. 브랜드를 담은 의미있는 모션이다. 하지만 그럼에도 이 모션이 사용자에게 의미가 있는지, 좋은 UX를 주는지는 의문이 든다.",
+          "\u00A0\u00A0\u00A0사실 이 모션은 쿠돈의 브랜드 키워드 3가지 '순환시키는, 도움이 되는, 재빠른' 중 '순환시키는'과 '재빠른'을 모션으로 실체화한 것이다. 브랜드를 담은 의미있는 모션이다. 하지만 이 모션이 사용자에게 의미가 있는지, 좋은 UX를 주는지는 의문이 든다.",
         e:
           "\u00A0\u00A0\u00A0In fact, this motion is the realization of 'circulating' and 'quick' among the three keywords of Koodon's brand keyword,'circulating, helpful, and quick'. It is a meaningful motion with a brand. However, it is still questionable whether this motion is meaningful to the user or gives a good UX.",
       },
@@ -197,13 +197,13 @@ export const uiUxDesign2 = () => {
       },
       {
         k:
-          '디지털 프로덕트에서 모션 디자인은 사용자의 시선을 끌어 사용자에게 꼭 필요한 정보를 전달하거나, 사용자의 행동에 반응을 보여줘 사용자가 마치 컴퓨터 키보드와 같은 물리적인 UI를 쓸 때와 본질적으로는 같은 경험을 하게 하는 중요한 인터랙션 디자인 방법론이라 생각합니다. 하지만 목적 없이 저의 내적 만족을 위해 디자인한 모션이 과연 사용자에게 어떤 경험을 줄 수 있을지는 아직 답을 내리지 못했습니다.',
+          '디지털 프로덕트에서 모션 디자인은 사용자의 시선을 끌어 사용자에게 꼭 필요한 정보를 전달하거나, 사용자의 행동에 반응을 보여줘 사용자가 마치 컴퓨터 키보드와 같은 물리적인 UI를 쓸 때와 본질적으로는 같은 경험을 하게 하는 중요한 인터랙션 디자인 방법론이라 생각합니다. 하지만 목적 없이 저의 내적 만족을 위해 디자인한 모션이 과연 사용자에게 어떤 경험을 줄 수 있을지에 대해서는 아직 답을 내리지 못했습니다.',
         e:
           "In digital products, motion design attracts the user's gaze and conveys essential information to the user, or responds to the user's actions, allowing the user to experience essentially the same as when using a physical UI such as a computer keyboard. I think this is an important interaction design methodology. However, I have not yet been able to give an answer as to what kind of experience the motion designed for my inner satisfaction without purpose can truly give users.",
       },
       {
         k:
-          '\u00A0\u00A0\u00A0물론 요즘에는 디지털 프로덕트 UI 디자인에서 모션이 있는 게 일반적이라, 모션이 없는 프로덕트를 보면 생동감이 없는 딱딱한 느낌을 받는 것도 사실입니다. 디자이너의 시각에서 모션이 화려한 프로덕트를 보면 정말 멋지고 저도 만들어보고 싶다 느끼지만, 과연 사용자도 그렇게 느낄까요?',
+          '\u00A0\u00A0\u00A0물론 요즘에는 디지털 프로덕트 UI 디자인에서 모션이 있는 게 일반적이라, 모션이 없는 프로덕트를 보면 생동감이 없는 딱딱한 느낌을 받는 것도 사실입니다. 디자이너의 시각에서 모션이 화려한 프로덕트를 보면 정말 멋지고 저도 만들어보고 싶다고 느끼지만, 과연 사용자에게는 어떤 영향을 미칠까요?',
         e:
           "\u00A0\u00A0\u00A0Of course, these days, it is common for digital product UI design to have motion, so it is true that when you look at a product without motion, you get a hard feeling without a sense of life. From a designer's perspective, looking at a product with colorful motion is really cool and I feel like I want to make it, but will users feel that way?",
       },
