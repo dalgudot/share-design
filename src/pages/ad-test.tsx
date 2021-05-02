72d03431aefcc92dcf4f4e983353189e14be4796
khkim <dalgudot@gmail.com>
2021-05-02 15:11:28 +0900

    adSense test 2

--- src/pages/ad-test.tsx
+++ src/pages/ad-test.tsx
@@ -4,9 +4,21 @@ import styled from 'styled-components';
 const AdTest = () => {
   return (
     <>
-      <Head>
-        <title>AdTest</title>
-      </Head>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8444228904401312"
+        crossOrigin="anonymous"
+      ></script>
+
+      <ins
+        className="adsbygoogle"
+        style={{ display: 'block' }}
+        data-ad-client="ca-pub-8444228904401312"
+        data-ad-slot="7786078722"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
     </>
   );
 };
