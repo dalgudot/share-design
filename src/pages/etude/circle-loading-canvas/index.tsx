c978642b14b881dd9a5a7ceb4d6ffa3ecb221ebe
khkim <dalgudot@gmail.com>
2021-05-12 21:02:32 +0900

    add class colorful-wave

--- src/pages/etude/circle-loading-canvas/index.tsx
+++
@@ -1,7 +0,0 @@
-import CircleLoadingCanvas from '../../../components/pages/etude/circle-loading-canvas';
-
-const CircleLoadingIndex = () => {
-  return <CircleLoadingCanvas />;
-};
-
-export default CircleLoadingIndex;
