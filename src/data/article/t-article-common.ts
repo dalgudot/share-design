e68dacc4e20579c85879f82bf4fd55bf9a3128c8
khkim <dalgudot@gmail.com>
2021-05-29 14:42:01 +0900

    ~

--- src/data/article/t-article-common.ts
+++ src/data/article/t-article-common.ts
@@ -1,7 +1,7 @@
 export const tArticleCommon = () => {
   return {
     message: {
-      k: '디자인과 개발 경험을 공유합니다 :)',
+      k: '디자인 경험을 공유합니다 :)',
       e: 'I share my experience :)',
     },
 
@@ -68,16 +68,12 @@ export const tArticleCommon = () => {
 
       iOSAPPDownloadContents: [
         {
-          k:
-            '디자인 공유하기는 데스크톱, 태블릿, 모바일 등 어떤 기기에서든 크롬이나 사파리 같은 웹 브라우저에 URL(share-design.kr)을 입력해 접속할 수 있습니다.',
-          e:
-            'SHARE DESIGN can be accessed by entering the URL(share-design.kr/en) in a web browser such as Chrome or Safari from any device, such as desktop, tablet, or mobile.',
+          k: '디자인 공유하기는 데스크톱, 태블릿, 모바일 등 어떤 기기에서든 크롬이나 사파리 같은 웹 브라우저에 URL(share-design.kr)을 입력해 접속할 수 있습니다.',
+          e: 'SHARE DESIGN can be accessed by entering the URL(share-design.kr/en) in a web browser such as Chrome or Safari from any device, such as desktop, tablet, or mobile.',
         },
         {
-          k:
-            '\u00A0\u00A0\u00A0다만 모바일 웹 브라우저에는 상단 주소 입력창과 하단 툴바(Toolbar)가 있어서 디자인 공유하기가 차지하는 공간이 좁아지고, 디자인 공유하기의 UX 디자인을 온전히 경험하기 힘듭니다. 이 문제를 해결하기 위해 iOS 앱을 개발했습니다. iOS 앱을 설치하면 모바일 환경에서 디자인 공유하기가 전달하고자 하는 UX 디자인을 온전히 경험할 수 있으며, 새 콘텐츠가 발행됐을 때 푸시 알림을 받을 수 있습니다.',
-          e:
-            '\u00A0\u00A0\u00A0However, the mobile web browser has an upper address input window and a lower toolbar, which reduces the space occupied by SHARE DESIGN, and it is difficult to fully experience the UX design of SHARE DESIGN. I have developed an iOS app to solve this problem. By installing the iOS app, you can fully experience the UX design that SHARE DESIGN on the mobile environment wants to convey, and you can receive push notifications when new content is published.',
+          k: '\u00A0\u00A0\u00A0다만 모바일 웹 브라우저에는 상단 주소 입력창과 하단 툴바(Toolbar)가 있어서 디자인 공유하기가 차지하는 공간이 좁아지고, 디자인 공유하기의 UX 디자인을 온전히 경험하기 힘듭니다. 이 문제를 해결하기 위해 iOS 앱을 개발했습니다. iOS 앱을 설치하면 모바일 환경에서 디자인 공유하기가 전달하고자 하는 UX 디자인을 온전히 경험할 수 있으며, 새 콘텐츠가 발행됐을 때 푸시 알림을 받을 수 있습니다.',
+          e: '\u00A0\u00A0\u00A0However, the mobile web browser has an upper address input window and a lower toolbar, which reduces the space occupied by SHARE DESIGN, and it is difficult to fully experience the UX design of SHARE DESIGN. I have developed an iOS app to solve this problem. By installing the iOS app, you can fully experience the UX design that SHARE DESIGN on the mobile environment wants to convey, and you can receive push notifications when new content is published.',
         },
       ],
 
@@ -88,16 +84,12 @@ export const tArticleCommon = () => {
 
       newNotificationContents: [
         {
-          k:
-            '디자인 공유하기에서 새 콘텐츠가 발행됐을 때 어떻게 알려드릴 수 있을지 고민이 많았습니다. 디자인 공유하기는 전 세계 사람들을 위한 공간이기 때문에, 전 세계 사람들에게 소식을 전달할 수 있어야 했습니다. 디자인 공유하기는 사용자의 어떤 정보도 수집하지 않기 때문에 이메일, SMS 등으로 알림을 드릴 수는 없었습니다.',
-          e:
-            "I had a lot of thoughts on how to let you know when new content was published in SHARE DESIGN. SHARE DESIGN is a space for people all over the world, so I needed to be able to reach people around the world. SHARE DESIGN doesn't collect any information from users, so I couldn't be notified by email, SMS, etc.",
+          k: '디자인 공유하기에서 새 콘텐츠가 발행됐을 때 어떻게 알려드릴 수 있을지 고민이 많았습니다. 디자인 공유하기는 전 세계 사람들을 위한 공간이기 때문에, 전 세계 사람들에게 소식을 전달할 수 있어야 했습니다. 디자인 공유하기는 사용자의 어떤 정보도 수집하지 않기 때문에 이메일, SMS 등으로 알림을 드릴 수는 없었습니다.',
+          e: "I had a lot of thoughts on how to let you know when new content was published in SHARE DESIGN. SHARE DESIGN is a space for people all over the world, so I needed to be able to reach people around the world. SHARE DESIGN doesn't collect any information from users, so I couldn't be notified by email, SMS, etc.",
         },
         {
-          k:
-            '\u00A0\u00A0\u00A0제가 생각해낸 아이디어는 유튜브나 인스타그램 같은 전 세계 사람들이 쓰는 플랫폼을 통해 소식을 전하는 방법이었습니다. 뉴스레터의 형식도 고민했었지만, 추후 비용이 발생할 가능성이 있어서 후보에서 제외했습니다. 최종적으로 선택한 방법은 인스타그램 피드를 통해 소식을 전하는 방법입니다. 이 인스타그램 계정에는 디자인 공유하기에 새 콘텐츠가 발행될 때만 피드가 게시될 예정입니다. 새 콘텐츠가 발행됐을 때 인스타그램 피드를 통해 알림을 받고 싶으신 분은 팔로우를 부탁드립니다.',
-          e:
-            '\u00A0\u00A0\u00A0The idea I came up with was a way to deliver news through platforms used by people around the world like YouTube and Instagram. I also thought about the format of the newsletter, but I excluded it from the nomination because there is a possibility of incurring costs in the future. The final choice was to share the news via Instagram feed. The feed will only be posted when new content is published on this Instagram account for SHARE DESIGN. If you would like to be notified via Instagram feed when new content is published, please follow me.',
+          k: '\u00A0\u00A0\u00A0제가 생각해낸 아이디어는 유튜브나 인스타그램 같은 전 세계 사람들이 쓰는 플랫폼을 통해 소식을 전하는 방법이었습니다. 뉴스레터의 형식도 고민했었지만, 추후 비용이 발생할 가능성이 있어서 후보에서 제외했습니다. 최종적으로 선택한 방법은 인스타그램 피드를 통해 소식을 전하는 방법입니다. 이 인스타그램 계정에는 디자인 공유하기에 새 콘텐츠가 발행될 때만 피드가 게시될 예정입니다. 새 콘텐츠가 발행됐을 때 인스타그램 피드를 통해 알림을 받고 싶으신 분은 팔로우를 부탁드립니다.',
+          e: '\u00A0\u00A0\u00A0The idea I came up with was a way to deliver news through platforms used by people around the world like YouTube and Instagram. I also thought about the format of the newsletter, but I excluded it from the nomination because there is a possibility of incurring costs in the future. The final choice was to share the news via Instagram feed. The feed will only be posted when new content is published on this Instagram account for SHARE DESIGN. If you would like to be notified via Instagram feed when new content is published, please follow me.',
         },
       ],
     },
--- src/pages/wave-test.tsx
+++ src/pages/wave-test.tsx
@@ -6,6 +6,23 @@ const WaveTest = () => {
     <>
       <Div />
       <WaveLoading />
+      {/* https://css-tricks.com/creating-patterns-with-svg-filters/ */}
+      <SVG xmlns="http://www.w3.org/2000/svg">
+        <filter id="noise__filter">
+          <feTurbulence //
+            baseFrequency={1}
+            // type="fractalNoise"
+            // numOctaves="2"
+          />
+          <feColorMatrix
+            values="0 0 0 9 -4
+                    0 0 0 9 -4
+                    0 0 0 9 -4
+                    0 0 0 0 1"
+          />
+        </filter>
+        <rect width="100%" height="100%" filter="url(#noise__filter)" />
+      </SVG>
     </>
   );
 };
@@ -19,4 +36,15 @@ const Div = styled.div`
   margin: 200px auto 0;
   border-radius: 50%;
   opacity: 0.5;
+`;
+
+const SVG = styled.svg`
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  opacity: 0.5;
+  z-index: -100;
 `;
