cd22fd40efd699126b9148db83310757ae9dbab6
khkim <dalgudot@gmail.com>
2021-09-12 18:19:47 +0900

    update

--- src/components/pages-components/product/share-colors/extract-colors.tsx
+++ src/components/pages-components/product/share-colors/extract-colors.tsx
@@ -47,14 +47,14 @@ const ExtractColors = ({ image }: { image: any }) => {
           {/* iOS에서는 문제가 많아서 모바일 전체에서 다운로드 버튼 숨기기 */}
           {/* isMobile -> returns true if device type is mobile or tablet */}
           {/* 데스크톱과 안드로이드 기기에서만 다운로드 가능 */}
-          {(!isMobile || isAndroid) && (
-            <motion.button
-              onClick={downloadImageAndPalette}
-              whileTap={{ scale: 0.8 }}
-            >
-              <H1Title text={{ k: '다운로드', e: 'Download' }} color="gray1" />
-            </motion.button>
-          )}
+          {/* {(!isMobile || isAndroid) && ( */}
+          {/* <motion.button
+            onClick={downloadImageAndPalette}
+            whileTap={{ scale: 0.8 }}
+          >
+            <H1Title text={{ k: '다운로드', e: 'Download' }} color="gray1" />
+          </motion.button> */}
+          {/* )} */}
         </Container>
       </>
     );
