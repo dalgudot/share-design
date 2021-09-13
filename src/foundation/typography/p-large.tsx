f7e94cdd7f36b649a914ded6b54afc64f9027eda
khkim <dalgudot@gmail.com>
2021-09-13 21:21:04 +0900

    SHARE PALETTE Devlopement in progress

--- src/foundation/typography/p-large.tsx
+++ src/foundation/typography/p-large.tsx
@@ -5,6 +5,7 @@ const PLarge = ({
   text,
   color,
   weight,
+  lineHeight,
   marginTop,
 }: {
   text:
@@ -15,6 +16,10 @@ const PLarge = ({
     | string;
   color: string;
   weight?: 100 | 300 | 400 | 700;
+  lineHeight?: {
+    desktop: string;
+    mobile: string;
+  };
   marginTop?: string;
 }) => {
   return (
@@ -27,10 +32,12 @@ const PLarge = ({
         desktop: '21px',
         mobile: '18px',
       }}
-      lineHeight={{
-        desktop: '36px', // 21px * 1.618 - based on the body text
-        mobile: '30px', // 18px * 1.618 - based on the body text
-      }}
+      lineHeight={
+        lineHeight || {
+          desktop: '36px', // 21px * 1.618 - based on the body text
+          mobile: '30px', // 18px * 1.618 - based on the body text
+        }
+      }
       letterSpacing={{
         desktop: '0px',
         mobile: '0px',
