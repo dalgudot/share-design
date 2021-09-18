7e5382eb49b1cafd4dca4a95508dcf74698a1e33
khkim <dalgudot@gmail.com>
2021-09-18 20:41:03 +0900

    add thumbnail image

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,8 +1,10 @@
 import { useEffect } from 'react';
 import { VisitsAndViewsDuringSession } from '../../lib/functions/visits-and-views';
 import styled from 'styled-components';
+import { useArvAmplitude } from '../../lib/hooks/Amplitude/useArvAmplitude';
 
 const ProductIndex = () => {
+  // useArvAmplitude('arv_product');
   useEffect(() => {
     VisitsAndViewsDuringSession('Product');
   }, []);
